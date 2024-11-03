981fde635fd5861bfab67db3f76d0dbe7a5885f4
Đinh Ngọc Lâm <dinhngoclam28386@gmail.com>
2024-11-04 00:46:09 +0700

    ok

---
+++ src/routers/product.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProduct,
+  getProductById,
+  updateProduct,
+} from "../controllers/product";
+import { checkAuth } from "../middlewares/checkauth";
+// import { getAllProduct } from "../controllers/product";
+
+const router = express.Router();
+
+router.get("/products", getAllProduct);
+router.get("/products/:id", getProductById);
+router.post("/products", checkAuth, createProduct);
+router.put("/products/:id", checkAuth, updateProduct);
+router.delete("/products/:id", checkAuth, deleteProduct);
+
+export default router;
